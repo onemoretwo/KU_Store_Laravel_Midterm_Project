ef9a371bb44611f75edffde278ba59996c4ed0c1
Arnon Suksiri <arnon.suksili@gmail.com>
2020-02-04 17:04:50 +0700

    edit123

--- resources/views/login/index.php
+++ resources/views/login/index.php
@@ -86,7 +86,7 @@
                                     </div>
                                 </div>
                                 <div class="d-flex justify-content-center mt-3 login_container">
-                                    <button type="button" name="button" class="btn login_btn" onclick="window.location.href = '/home';">Login</button>
+                                    <button type="button" name="button" class="btn login_btn">Login</button>
                                 </div>
                             </form>
                         </div>
