529391afc89a2481b97446c09860eb637236cf47
6110406147 <phitchayatida.c@ku.th>
2020-02-05 16:46:45 +0700

    login

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -23,11 +23,16 @@ class LoginController extends Controller {
                 ]);
                 return $this->redirect('cart');
             }else{
-                return "incorrect password";
+               echo "<script>alert('Sorry, Your password is incorrect.')</script>";
             }
+        }else if($user!= $username){
+            echo "<script>alert('Sorry, Your username is incorrect.')</script>";
+            
         }else{
-            return "incorrect username";
+            echo "<script>alert('Please fill all inputs')</script>";
         }
+        echo "<script>window.location.href = '/login'</script>";
+
     }
 
     public function signup(){
@@ -38,7 +43,8 @@ class LoginController extends Controller {
         $rpwd = $input->rpwd;
 
         if(empty($username) && empty($email) && empty($npwd) && empty($rpwd)){
-            return "No input";
+            echo "<script>alert('All fields are required.')</script>";
+            
         }
 
         if(!empty($username)){
@@ -49,17 +55,21 @@ class LoginController extends Controller {
                         $result = (new User())->create_user($username,$email,$hash);
                         return $this->redirect('/login');
                     }else{
-                        return "password doesn't match";
+                        echo "<script>alert('Password doesn't match')</script>";
+                        
                     }
                 }else{
-                    return "warning: empty password";
+                    echo "<script>alert('Empty password')</script>";
+                    
                 }
             }else{
-               return "Email : this field doesn't empty"; 
+                echo "<script>alert('Please fill your E-mail')</script>";
+               
             }
         }else{
-            return "username: this field doesn't empty";
+            echo "<script>alert('Please fill your Username')</script>";
         }
+        echo "<script>window.location.href = '/login'</script>";
     }
 
     public function logout() {
