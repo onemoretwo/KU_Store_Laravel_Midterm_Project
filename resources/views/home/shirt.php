47185eddd479ac28944515952e90a9ad574e120f
6110406147 <phitchayatida.c@ku.th>
2020-02-06 08:04:21 +0700

    ban edit

--- resources/views/home/shirt.php
+++ resources/views/home/shirt.php
@@ -27,33 +27,6 @@
 
 <body id="page-top">
 
-
- <!-- <div class=" carouse">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner" style="height: 450px;">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="/images/cou2.jpg" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="/images/cou3-1.jpg" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="/images/cou1.jpg" alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>   -->
-
-
-
-
     <!-- Portfolio Grid -->
     <section class="bg-light page-section" id="portfolio">
         <div class="container">
@@ -224,6 +197,7 @@
             </div>
         </div>
     </div>
+<?php endforeach; ?>
 
 
     <!-- Bootstrap core JavaScript -->
