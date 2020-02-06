f9985dd7040013204053689851102b51c541de49
6110406147 <phitchayatida.c@ku.th>
2020-02-06 13:32:46 +0700

    Update DashController.php

--- src/Controllers/DashController.php
+++ src/Controllers/DashController.php
@@ -4,10 +4,7 @@ namespace App\Controllers;
 
 class DashController extends Controller {
     public function index() {
-        $auth = Session::read('Auth');
-        $usersuse = (new Point_log())->point_log_use($auth['id']);
-        $usersget = (new Point_log())->point_log_get($auth['id']);
-        return $this->render('edit/index', ['usersget' => $usersget,'usersuse' => $usersuse]);
+        return $this->render('dash/index');
     }
     
 }
